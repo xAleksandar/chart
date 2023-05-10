6a69e12ec0a9dc0d094bc4434b4681c6ba84d06d
alex <aleksandar@kalaydzhiev.com>
2023-05-10 16:34:11 +0300

    Added comments and optimizations

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const data = [
-{ x: 10, y: 20, value: 50 },
-{ x: 40, y: 60, value: 80 },
-{ x: 70, y: 30, value: 20 },
-{ x: 30, y: 60, value: 60 },
-{ x: 55, y: 80, value: 50}
+{ x: 10, y: 20, value: 25 },
+{ x: 40, y: 60, value: 40 },
+{ x: 70, y: 30, value: 10 },
+{ x: 30, y: 60, value: 30 },
+{ x: 55, y: 80, value: 25}
 ];
 
 const canvas = document.getElementById('chart');
 const ctx = canvas.getContext('2d');
-
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+
+// Configuring chart dimensions
 const margin = 40;
-// const chartWidth = canvas.width;
-// const chartHeight = canvas.height;
 const chartWidth = canvas.width - 2 * margin;
 const chartHeight = canvas.height - 2 * margin;
 
 
 // Draw grid lines
 ctx.beginPath();
+
 for (let i = 0; i < 6; i++) {
     const x = margin + i * (chartWidth / 5);
     ctx.moveTo(x, margin);
@@ -33,6 +33,7 @@ for (let i = 0; i < 6; i++) {
 ctx.strokeStyle = '#ddd';
 ctx.stroke();
 
+
 // Draw axes
 ctx.beginPath();
 ctx.moveTo(margin, margin);
@@ -40,6 +41,7 @@ ctx.lineTo(margin, canvas.height - margin);
 ctx.lineTo(canvas.width - margin, canvas.height - margin);
 ctx.strokeStyle = '#000';
 ctx.stroke();
+
 
 // Draw axis labels
 ctx.fillStyle = '#000';
@@ -56,19 +58,18 @@ for (let i = 0; i < 6; i++) {
     ctx.fillText(label_y, margin / 4 , y);
 }
 
-// Find minimum and maximum values
+
+// Calculate minimum and maximum values
 const minValue = Math.min(...data.map((point) => point.value));
 const maxValue = Math.max(...data.map((point) => point.value));
+
 
 // Draw data points
 data.map((point) => {
     const x = margin + (point.x / 100) * chartWidth;
     const y = canvas.height - margin - (point.y / 100) * chartHeight;
-    const size = (point.value - minValue) / (maxValue - minValue) * 20 + 5;
-
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, 2 * Math.PI);
+    ctx.arc(x, y, point.value, 0, 2 * Math.PI);
     ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
     ctx.fill();
 });
-
